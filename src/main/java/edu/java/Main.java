a9df49abdbd51635744d3e884c9c389e9a1db397
ydvorzhetskiy <dvorzhetskij@mail.ru>
2021-02-04 20:21:18 +0600

    Main

--- src/main/java/edu/java/Main.java
+++ src/main/java/edu/java/Main.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        List<Integer> list = new LinkedList<>();
+
         for (int i = 0; i < 1_000_000; ++i) {
+            List<Integer> list = new LinkedList<>();
             Thread.sleep(1000);
             for (int j = 0; j < 1_000_000; ++j) {
                 list.add(j);
